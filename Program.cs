ac4f43e1e6ae8ab8e3d03d643a3c2d2e1deaa74b
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 19:28:22 -0600

    Programa Creado!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Escriba un programa que muestre la tabla de multiplicar del 5 utilizando do-while.
