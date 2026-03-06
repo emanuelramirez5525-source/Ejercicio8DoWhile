1eb2b911d0fc15eb5093d6bb086c4cbea31bcd0d
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 19:48:56 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿//Escriba un programa que muestre la tabla de multiplicar del 5 utilizando do-while.
+﻿// Escriba un programa que muestre la tabla de multiplicar del 5 utilizando do-while.
+
+int num = 5;
+int tabla = 1;
+
+Console.WriteLine("Tabla del 5");
+do
+{
+    
+    Console.WriteLine($"{num} X {tabla} = {num * tabla}");
+    tabla++;
+} while (tabla <= 10);
+
